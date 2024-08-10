701c27d85f061ef22ed6c9415654ce098644b0de
ms-online <27732357@qq.com>
2024-08-10 15:35:01 +1000

    react-course-2024

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import HomePage from './Pages/HomePage'
+import MainLayout from './Layouts/MainLayout'
+import JobsPage from './Pages/JobsPage'
+import React from 'react'
+import NotFoundPage from './Pages/NotFoundPage'
+import JobPage, { jobLoader } from './Pages/JobPage'
+import AddJobPage from './Pages/AddJobPage'
+import EditJobPage from './Pages/EditJobPage'
+
+
+const App = () => {
+  //add New Job
+  const addJob = async (newJob) => {
+
+    const res = await fetch('/api/jobs', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newJob),
+    });
+    return;
+  };
+
+  //Delete Job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+  };
+  //updateJob
+  const updateJob = async (job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
+
+  // initial 
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/add-job' element={<AddJobPage addJobSubmit={addJob} />} />
+      <Route
+        path='/edit-job/:id'
+        element={<EditJobPage updateJobSubmit={updateJob} />}
+        loader={jobLoader}
+      />
+      <Route
+        path='/jobs/:id'
+        element={<JobPage deleteJob={deleteJob} />}
+        loader={jobLoader}
+      />
+      <Route path='*' element={<NotFoundPage />} />
+    </Route>))
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
